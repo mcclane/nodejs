c179db52c3d26ae65e928f2ef63bd82062d9caea
McClane Howland <mcclane.howland@gmail.com>
2017-10-15 10:48:17 -0700

    adfa

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-//var urlencodedParser = bodyParser.urlencoded({extended: false});
-//var jsonParser = bodyParser.json();
+var express = require('express')
+var fs = require('fs')
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+var jsonParser = bodyParser.json()
 
-var PORT = 3000;
+var PORT = 3000
 
 //create express instance
-var app = express();
+var app = express()
 
 //middle ware to handle sessions
-//app.use(urlencodedParser);
-//app.use(jsonParser);
+app.use(urlencodedParser)
+app.use(jsonParser)
 //USE A FAVICON app.use(favicon(__dirname + '/favicon.ico'));
 
 //HOW TO SERVE STATIC FILES for mcclane
@@ -19,15 +19,23 @@ var app = express();
 
 //ROUTES
 
+//REGULAR PAGE SERVING
 //our home page, these lines are in every node server
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/html/index.html');
-});
+    res.sendFile(__dirname + '/html/index.html')
+})
 
 app.get('/css/style.css', function(req, res) {
-    res.sendFile(__dirname + '/css/style.css');
-});
+    res.sendFile(__dirname + '/css/style.css')
+})
+
+//put the API/processing code here when you've come up with it
+//listen for some requests on like api.tlanews.com (create a new record in route 53) and make sure what is submitted has a paypal thing to go with it. You also need a paypal business account for it to work, so get Mom to help.
+app.post('/test', function(req, res) {
+    console.log("Something recieved")
+})
+
 
 //START THE SERVER
-app.listen(PORT);
+app.listen(PORT)
 console.log('server running on port: ' + PORT)
